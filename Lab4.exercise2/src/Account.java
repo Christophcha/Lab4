334b58252530b75229e8228505ee4390884ef6bf
Christophcha <39505761+Christophcha@users.noreply.github.com>
2021-07-26 17:35:06 -0500

    update

--- Lab4.exercise2/src/Account.java
+++ Lab4.exercise2/src/Account.java
@@ -2,26 +2,25 @@
 public class Account {
 
 	static int numAccounts = 0;
-	
+
 	public static void main(String[] args) {
-		
+
 		Account object1 = new Account();
 		Account object2 = new Account();
 		Account object3 = new Account();
 		Account object4 = new Account();
 		Account object5 = new Account();
 		Account object6 = new Account();
-		
-		System.out.println(getNumAccounts()); //prints out number of accounts
+
+		System.out.println("Accounts Created: " + getNumAccounts()); // prints out number of accounts
 	}
-	
+
 	public static int getNumAccounts() {
 		return numAccounts;
 	}
-	public Account(){
-		numAccounts = numAccounts+1; //add 1 per acc
-	}
 
-	
+	public Account() {
+		numAccounts = numAccounts + 1; // add 1 per acc
+	}
 
 }
